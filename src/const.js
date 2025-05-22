db71f6c67f6bebed00fb1c5214f21036a177a098
Oksana T. <pochtoviy-yashik@mail.ru>
2025-05-22 16:42:29 +0400

    navigation and service-request image

--- src/const.js
+++ src/const.js
@@ -16,7 +16,6 @@ const children = Object.keys(groupModelsByBrand).reduce((acc, key) => {
 	acc[key] = groupModelsByBrand[key].map(model => ( { url: `models/${model.id}/`, name: `${model.name.toUpperCase()}`, thumb: model.thumb } ) );
 	return acc;
 }, {});
-import modelsData from '@/data/models.json';
 const models = modelsData.filter(model => model.show);
 export const LINKS_MENU = [
 	{url: 'cars/', name: 'Авто в наличии'},
@@ -25,7 +24,7 @@ export const LINKS_MENU = [
 	{ 
 		url: 'models/', 
 		name: 'Модели',
-		children: models.map(model => ( { url: `models/${model.id}/`, name: `${model?.mark_id} ${model.name.toUpperCase()}` } ) )
+		children
 	},
 	// {url: 'trade-in/', name: 'Оценка автомобиля'},
 	// {url: 'special-offers/', name: 'Спецпредложения'},
